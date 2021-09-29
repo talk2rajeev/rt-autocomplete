46b42ace1ab5c78a639acd8a9562330f90ae7b4b
Rajeev Sharma <talk2rajeev1986@gmail.com>
2021-09-29 18:20:41 +0530

    custom listItem with image + ReadMe updated

--- README.md
+++ README.md
@@ -84,18 +84,26 @@ export default App;
 
 ### props
 
-```javascript
-type OptionValue = string | number;
+```Typescript
+export type Option = {
+  id: string;
+  value: string;
+  img?: string;
+  secondaryValue?: string;
+};
 
-interface Props<T extends OptionValue> {
-    onkeypress: (query: string, delay?: number) => void;
-    onblur?: (query: string) => void;
-    onfocus?: () => void;
-    listItems: Option<T>[];
-    defaultValue?: string,
-    maxItem?: number,
-    showDropdownIcon?: boolean
-    CustomDropdownIcon?: React.ReactElement;
+interface Props {
+  onkeypress: (query: string, delay?: number) => void;
+  onblur?: (query: string) => void;
+  onfocus?: () => void;
+  onclickmore?: (query: string) => void;
+  onSelect: (item: Option) => void;
+  renderCustomListItem?: (porps: any) => React.ReactElement;
+  listItems: Option[];
+  defaultValue?: string;
+  maxItem?: number;
+  showDropdownIcon?: boolean;
+  CustomDropdownIcon?: React.ReactElement;
 }
 ```
 
@@ -103,16 +111,131 @@ interface Props<T extends OptionValue> {
 
 ```
 
-| props              | type          | description                             |
-| ------------------ |:-------------:| ---------------------------------------:|
-| onkeypress         | function      | query: string, delay: number (optional) |
-| onblur             | function      | optional, (query: string)               |
-| onfocus            | function      | optional,                               |
-| listItems          | Array[]       | { value: string | number, id: string }  |
-| defaultValue       | string        | optional, default value to pre-populate |
-| maxItem            | number        | optional, show **more items link** if no| 
-|                    |               | of items are large                      |
-| showDropdownIcon   | boolean       | optional, show/hide icon                |
-| CustomDropdownIcon | ReactElement  | optional,                               |
+| props                | type          | description                               |
+| -------------------- |:-------------:| -----------------------------------------:|
+| onkeypress           | function      | query: string, delay: number (optional)   |
+| onblur               | function      | [optional], (query: string)               |
+| onfocus              | function      | [optional],                               |
+| onclickmore          | function      | [optional], (query: string)               |
+| onSelect             | function      | (item: Option) => void;                   |
+| renderCustomListItem | function      | [optional], (porp: Option)=> ReactElement |
+| listItems            | Array[]       | { value: string | number, id: string }    |
+| defaultValue         | string        | [optional], default value to pre-populate |
+| maxItem              | number        | [optional], show "more items link" if no  | 
+|                      |               | of items are large                        |
+| showDropdownIcon     | boolean       | [optional], show/hide icon                |
+| CustomDropdownIcon   | ReactElement  | [optional],                               |
+
+```
+
+
+### React + Typescript example
+
+```tsx
+import React, { useState } from 'react';
+import { AutoComplete, Option }  from './rt-autocomplete'; // Option is type of List Item
+
+const CustomListItem = (props: Option) => {
+    return (
+        <div className="">
+            {props.value}
+        </div>
+    )        
+}
+
+const CustomListItemWithImage = (props: Option) => {
+    return (
+        <div className="listitem">
+            <div style={{display: 'grid', gridTemplateColumns: '50px auto', gridTemplateRows: 35}}>
+                <div><img src={props.img} alt="" style={{borderRadius: '50%', height: 40}}/></div>
+                <div>
+                    <div style={{fontSize: 14}}>{props.value}</div>
+                    <div style={{fontSize: 12, color: '#777'}}>{props.secondaryValue}</div>
+                </div>
+            </div>
+        </div>
+    )        
+}
+
+export default function App() {
+    const [list, setList] = useState<Option[]>([])
+    const [userList, setUserList] = useState<Option[]>([])
+
+    const userImg =[
+        "https://randomuser.me/api/portraits/thumb/men/46.jpg",
+        "https://randomuser.me/api/portraits/thumb/women/28.jpg",
+        "https://randomuser.me/api/portraits/thumb/men/4.jpg",
+        "https://randomuser.me/api/portraits/thumb/men/93.jpg",
+        "https://randomuser.me/api/portraits/thumb/women/66.jpg",
+        "https://randomuser.me/api/portraits/thumb/men/90.jpg",
+        "https://randomuser.me/api/portraits/thumb/women/43.jpg",
+        "https://randomuser.me/api/portraits/thumb/men/92.jpg",
+        "https://randomuser.me/api/portraits/thumb/men/32.jpg",
+        "https://randomuser.me/api/portraits/thumb/women/58.jpg"
+    ]
+
+    const onkeypress = (query: string) => {
+        fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(res => res.json())
+            .then(result => {
+                const filteredList = result.filter((item: any) => item.email.includes(query.toLowerCase())).map((item: any) => ({id: item.id, value: item.email}))
+                setList(filteredList)
+            })
+    }
+
+    const onchange = (query: string) => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+            .then(result => {
+                const filteredUserList: Option[] = result.filter((item: any) => item.name.toLowerCase().includes(query.toLowerCase())).map((item: any) => ({id: item.id, value: item.name, img: userImg[Math.floor(Math.random()*10)], secondaryValue: item.email }))
+                setUserList(filteredUserList)
+            })
+    }
+
+    const onSelect = (item: Option) => {
+        console.log(item.id, item.value)
+    }
+
+    
+    
+    return (
+        <div className="your-classname">
+            <div className="container">
+                <h3>Auto complete</h3>
+                <AutoComplete 
+                    onkeypress = {onkeypress}
+                    listItems = { list }
+                    maxItem={15}
+                    showDropdownIcon = {true}
+                    onSelect={onSelect}
+                />
+            </div>
+
+            <div className="container">
+                <h3>Auto complete with CustomListItem</h3>
+                <AutoComplete 
+                    onkeypress = {onkeypress}
+                    listItems = { list }
+                    maxItem={15}
+                    showDropdownIcon = {true}
+                    renderCustomListItem={CustomListItem}
+                    onSelect={onSelect}
+                />
+            </div>
+
+            <div className="container">
+                <h3>Auto complete with CustomListItem(image + text)</h3>
+                <AutoComplete 
+                    onkeypress = {onchange}
+                    listItems = { userList }
+                    maxItem={15}
+                    showDropdownIcon = {true}
+                    renderCustomListItem={CustomListItemWithImage}
+                    onSelect={onSelect}
+                />
+            </div>
+        </div>
+    );
+}
 
 ```
--- src/autocomplete/index.tsx
+++ src/autocomplete/index.tsx
@@ -1,134 +1,170 @@
-import React, { useRef, useState, useEffect } from 'react'
-import './autocomplete.css'; //
+import React, { useRef, useState, useEffect } from "react";
+import "./autocomplete.css";
 
-/* option value can be of a type string OR number */
-type OptionValue = string | number;
-
-type Option<T extends OptionValue> = {
-  value: T;
+export type Option = {
   id: string;
-}; 
+  value: string;
+  img?: string;
+  secondaryValue?: string;
+};
 
-interface Props<T extends OptionValue> {
-    onkeypress: (query: string, delay?: number) => void;
-    onblur?: (query: string) => void;
-    onfocus?: () => void;
-    onclickmore?: (query: string) => void;
-    listItems: Option<T>[];
-    listItemElement?: React.ReactElement,
-    defaultValue?: string,
-    maxItem?: number,
-    showDropdownIcon?: boolean,
-    CustomDropdownIcon?: React.ReactElement,
+interface Props {
+  onkeypress: (query: string, delay?: number) => void;
+  onblur?: (query: string) => void;
+  onfocus?: () => void;
+  onclickmore?: (query: string) => void;
+  onSelect: (item: Option) => void;
+  renderCustomListItem?: (porps: any) => React.ReactElement;
+  listItems: Option[];
+  defaultValue?: string;
+  maxItem?: number;
+  showDropdownIcon?: boolean;
+  CustomDropdownIcon?: React.ReactElement;
 }
 
-export const AutoComplete = <T extends OptionValue>({
-    onkeypress,
-    onblur,
-    onfocus,
-    onclickmore,
-    listItems,
-    listItemElement,
-    defaultValue="",
-    maxItem,
-    showDropdownIcon=false,
-    CustomDropdownIcon
-}: Props<T>) => {
-    const [isActive, setIsActive] = useState(false)
-    const [inputValue, setInputValue] = useState(defaultValue)
+export const AutoComplete = ({
+  onkeypress,
+  onblur,
+  onfocus,
+  onclickmore,
+  onSelect,
+  listItems,
+  renderCustomListItem,
+  defaultValue = "",
+  maxItem,
+  showDropdownIcon = false,
+  CustomDropdownIcon,
+}: Props) => {
+  const [isActive, setIsActive] = useState(false);
+  const [inputValue, setInputValue] = useState(defaultValue);
 
-    const inputRef = useRef<HTMLInputElement>(null)
-    const wrapperRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null);
+  const wrapperRef = useRef<HTMLInputElement>(null);
 
-    
-    const onInputFocus = () => {
-        setIsActive(true)
-        onfocus && onfocus()
+  const onInputFocus = () => {
+    setIsActive(true);
+    onfocus && onfocus();
+  };
+
+  const onDropdownIconClick = () => {
+    onInputFocus();
+    inputRef.current?.focus();
+  };
+
+  const onInputBlur = () => {
+    onblur && onblur(inputValue);
+  };
+
+  const onchange = (value: string) => {
+    setInputValue(value);
+    onkeypress(value);
+  };
+
+  const onItemSelect = (item: Option): void => {
+    if (item.value) {
+      setInputValue(item.value);
+      onSelect(item);
     }
+    setIsActive(false);
+  };
 
-    const onDropdownIconClick = () => {
-        onInputFocus();
-        inputRef.current?.focus();
+  const clickAnywhere = (e: any) => {
+    console.log('clickAnywhere called')
+    if (wrapperRef.current && !wrapperRef.current!.contains(e.target)) {
+      setIsActive(false);
     }
+  };
 
-    const onInputBlur = () => {
-        onblur && onblur(inputValue)
-    }
+  const onClickMoreLink = () => {
+    onclickmore
+      ? onclickmore(inputValue)
+      : console.warn(
+          'Please provide "onclickmore" function as props to <AutoComplete /> component'
+        );
+    setInputValue("");
+    setIsActive(false);
+  };
 
-    const onchange = (value: string) => {
-        setInputValue(value);
-        onkeypress(value)
-    }
+  useEffect(() => {
+    document.addEventListener("click", clickAnywhere);
 
-    const onItemSelect = (element: any): void => {
-        const dataid = element.target.attributes['data-id']
-        const dataval = element.target.attributes['data-val']
-        if (dataval) {
-            setInputValue(dataval.value)
-        } else {
-            // props.onMoreClick()
-        }
-        setIsActive(false)
-    }
+    return () => {
+      document.removeEventListener("click", clickAnywhere);
+    };
+  }, []);
 
-    const clickAnywhere = (e: any) => {
-        if (!wrapperRef.current!.contains(e.target)) {
-            setIsActive(false)
-        }
-    }
+  const listItemsToDisplay = listItems.slice(
+    0,
+    maxItem ? maxItem : listItems.length - 1
+  );
+  const remainingItems: number = maxItem ? listItems.length - maxItem : 0;
 
-    const onClickMoreLink = () => {
-        onclickmore ? onclickmore(inputValue) : console.warn('Please provide "onclickmore" function as props to <AutoComplete /> component');
-        setInputValue('');
-    }
-
-    useEffect(()=>{
-        document.addEventListener('click', clickAnywhere)
-
-        return () => {
-            document.removeEventListener('click', clickAnywhere);
-        }
-
-    }, [])
-
-    const listItemsToDisplay = listItems.slice(0, maxItem ? maxItem : listItems.length-1)
-    const remainingItems: number = maxItem ? listItems.length - maxItem : 0
-
-    return (
-        <div ref={wrapperRef} className="ddCoContainer">
-            <div className="inputContainer">
-                <input 
-                    ref = {inputRef}
-                    className="autocompleteinput"
-                    onChange = {(e) => onchange(e.target.value)}
-                    onFocus={onInputFocus}
-                    onBlur={onInputBlur}
-                    value={inputValue}
-                />
-                {
-                    showDropdownIcon && !CustomDropdownIcon ? <DefaultDropdownIcon onDropdownIconClick={onDropdownIconClick} />  : <span className="dropdown-icon" onClick={onDropdownIconClick}>{ CustomDropdownIcon }</span>
-                }
+  return (
+    <div ref={wrapperRef} className="ddCoContainer">
+      <div className="inputContainer">
+        <input
+          ref={inputRef}
+          className="autocompleteinput"
+          onChange={(e) => onchange(e.target.value)}
+          onFocus={onInputFocus}
+          onBlur={onInputBlur}
+          value={inputValue}
+        />
+        {showDropdownIcon && !CustomDropdownIcon ? (
+          <DefaultDropdownIcon onDropdownIconClick={onDropdownIconClick} />
+        ) : (
+          <span className="dropdown-icon" onClick={onDropdownIconClick}>
+            {CustomDropdownIcon}
+          </span>
+        )}
+      </div>
+      {isActive && listItemsToDisplay.length > 0 && (
+        <div className="listContainer">
+          {listItemsToDisplay.map((item) => (
+            <div onClick={() => onItemSelect(item)} key={item.id}>
+              {renderCustomListItem ? (
+                renderCustomListItem({ ...item })
+              ) : (
+                <ListItem item={item} />
+              )}
             </div>
-            { isActive && listItemsToDisplay.length > 0 && <div className="listContainer" onClick={(e)=>onItemSelect(e)}>
-                {
-                    listItemsToDisplay.map(item => <div className="listitem" key={item.id} data-val={item.value} data-id={item.id}>{item.value}</div>)
-                }
-                {
-                    remainingItems > 0 && listItemsToDisplay.length > 0 && <div className="more-item-text"><span className="more-link" onClick={onClickMoreLink}>{remainingItems} more items.</span></div>
-                }
-            </div> }
+          ))}
+          {remainingItems > 0 && listItemsToDisplay.length > 0 && (
+            <div className="more-item-text">
+              <span className="more-link" onClick={onClickMoreLink}>
+                {remainingItems} more items &raquo;
+              </span>
+            </div>
+          )}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 interface DropdownProps {
-    onDropdownIconClick: ()=> void;
-} 
-const DefaultDropdownIcon = ({onDropdownIconClick}:DropdownProps) => <svg className="dropdown-icon" onClick={onDropdownIconClick} width="24.000000000000004" height="24.000000000000004" xmlns="http://www.w3.org/2000/svg">
+  onDropdownIconClick: () => void;
+}
+const DefaultDropdownIcon = ({ onDropdownIconClick }: DropdownProps) => (
+  <svg
+    className="dropdown-icon"
+    onClick={onDropdownIconClick}
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <g id="Layer_1">
-        <title>Layer 1</title>
-        <path stroke="#999" id="svg_1" d="m6.58168,17.67989l6.17256,-11.14143l6.17256,11.14143l-12.34513,0l0.00001,0z" fill="#999"/>
+      <title>Layer 1</title>
+      <path
+        stroke="#999"
+        id="svg_1"
+        d="m6.58168,17.67989l6.17256,-11.14143l6.17256,11.14143l-12.34513,0l0.00001,0z"
+        fill="#999"
+      />
     </g>
-</svg>
+  </svg>
+);
 
-
+const ListItem: React.FC<{ item: Option }> = ({ item }) => (
+  <div className="listitem">{item.value}</div>
+);
